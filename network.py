933a25169927810b3aa1fb2b82d447f3ed1e2598
amusgrave-via <amusgrave@viascience.com>
2023-11-16 15:25:04 -0800

    wip

--- network.py
+++ network.py
@@ -26,6 +26,10 @@ pp.create_ext_grid(net, 1, vm_pu=1)
 pp.runpp(net)
 z_base = (400**2)/1e6
 
+netmodel = pp.create.create_empty_network()
+netmodel.bus=net.bus
+netmodel.load=net.load
+netmodel.line=net.line
 
 # Extract tranformer impedence from Ybus
 Ybus = net._ppc["internal"]["Ybus"].todense()
@@ -34,18 +38,18 @@ rt = zt.real
 xt = zt.imag
 
 # Replace tranformer impedence with equivalent line
-pp.create_line_from_parameters(net, from_bus=1, to_bus=2, length_km=1, r_ohm_per_km=z_base*rt, x_ohm_per_km=z_base*xt, c_nf_per_km=0, max_i_ka=1.0, index=-1)
-net.line.index = net.line.index+1
-net.line.sort_index(inplace=True)
+pp.create_line_from_parameters(netmodel, from_bus=1, to_bus=2, length_km=1, r_ohm_per_km=z_base*rt, x_ohm_per_km=z_base*xt, c_nf_per_km=0, max_i_ka=1.0, index=-1)
+netmodel.line.index = netmodel.line.index+1
+netmodel.line.sort_index(inplace=True)
 
 
-n = len(net.line)
+n = len(netmodel.line)
 A = np.zeros((n, n))
 
 A[0, 0] = -1
 for i in range(1, n):
-    A[i, net.line.iloc[i-1].from_bus-1]=1
-    A[i, net.line.iloc[i-1].to_bus-1]=-1
+    A[i, netmodel.line.iloc[i-1].from_bus-1]=1
+    A[i, netmodel.line.iloc[i-1].to_bus-1]=-1
     
 F = np.linalg.inv(A)
 
@@ -53,21 +57,21 @@ F = np.linalg.inv(A)
 
 #inediagr = (1/z_base)*np.diag(net.line.r_ohm_per_km*net.line.length_km)
 #diagr = np.block([[rt, np.zeros((1, n-1))],[np.zeros((n-1, 1)), linediagr]])
-Dr = (1/z_base)*np.diag(net.line.r_ohm_per_km*net.line.length_km)
+Dr = (1/z_base)*np.diag(netmodel.line.r_ohm_per_km*netmodel.line.length_km)
 R = F @ Dr @ F.T
 print(R)
 
 #linediagx = (1/z_base)*np.diag(net.line.x_ohm_per_km*net.line.length_km)
 #diagx = np.block([[xt, np.zeros((1, n-1))],[np.zeros((n-1, 1)), linediagr]])
 
-Dx = (1/z_base)*np.diag(net.line.x_ohm_per_km*net.line.length_km)
+Dx = (1/z_base)*np.diag(netmodel.line.x_ohm_per_km*netmodel.line.length_km)
 X = F @ Dx @ F.T
 print(X)
 
 P = np.zeros((n, 1))
 Q = np.zeros((n, 1))
 
-for b, p, q in zip(net.load.bus, net.load.p_mw, net.load.q_mvar):
+for b, p, q in zip(netmodel.load.bus, netmodel.load.p_mw, netmodel.load.q_mvar):
     P[b-2]=p
     Q[b-2]=q
 
