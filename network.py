e092be140b79661eb19a316c7403a6d5e00e1c98
amusgrave-via <amusgrave@viascience.com>
2023-11-16 16:13:49 -0800

    new network model with transformer converted to line

--- network.py
+++ network.py
@@ -48,22 +48,14 @@ A = np.zeros((n, n))
 
 A[0, 0] = -1
 for i in range(1, n):
-    A[i, netmodel.line.iloc[i-1].from_bus-1]=1
-    A[i, netmodel.line.iloc[i-1].to_bus-1]=-1
+    A[i, netmodel.line.iloc[i].from_bus-2]=1
+    A[i, netmodel.line.iloc[i].to_bus-2]=-1
     
 F = np.linalg.inv(A)
-
-
-
-linediagr = (1/z_base)*np.diag(net.line.r_ohm_per_km*net.line.length_km)
-diagr = np.block([[rt, np.zeros((1, n-1))],[np.zeros((n-1, 1)), linediagr]])
 
 Dr = (1/z_base)*np.diag(netmodel.line.r_ohm_per_km*netmodel.line.length_km)
 R = F @ Dr @ F.T
 print(R)
-
-linediagx = (1/z_base)*np.diag(net.line.x_ohm_per_km*net.line.length_km)
-diagx = np.block([[xt, np.zeros((1, n-1))],[np.zeros((n-1, 1)), linediagx]])
 
 Dx = (1/z_base)*np.diag(netmodel.line.x_ohm_per_km*netmodel.line.length_km)
 X = F @ Dx @ F.T
@@ -87,5 +79,5 @@ print(vlin)
 
 vmod = net.res_bus.vm_pu[1:].array.reshape(n, 1)
 
-error = 100*(vmod-vlin)/(np.ones((n, 1))-vlin)
+error = 100*(vlin-vmod)/(np.ones((n, 1))-vlin)
 print(error)
